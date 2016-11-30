ce69b7a7f7f037de14471cee7fb39fc4a14f105b
Lionel Jouin <lionel.jouin@outlook.com>
2016-11-30 22:06:28 +0100

    init project

---
+++ api/models/Line.js
@@ -0,0 +1,22 @@
+/**
+ * Line.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    values: {
+      type: 'json'
+    },
+
+    inventory: {
+      model: 'inventory'
+    }
+
+  }
+};
+
